f3bd96a3f00bc42ec1b7e916bee2b00026e7dd2d
Vinícius <vinicius.ferreira@ufpe.br>
2025-09-28 11:54:27 -0300

    ch2-present-yourself: marcação e estilização

---
+++ ch2/present-yourself/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <img src="" alt="" />
+        <h1>Vinícius Ferreira</h1>
+        <p>Desenvolvimento Web & Análise de Dados</p>
+      </header>
+      <main>
+        <p>
+          Analista de Sistemas com especialização em Estatística, Engenharia e
+          Ciência de Dados.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
